af66755b93164d6da23bc42a6ab23280c639b44b
akucoba231 <aku.coba231@gmail.com>
2025-09-24 12:11:31 +0700

    Update script.js

--- script.js
+++ script.js
@@ -957,6 +957,7 @@ $(document).ready(function() {
     function hapusBannerOveride(){
         let hapusBanner = document.getElementById('hapus-banner')
         hapusBanner.style.display = "none";
+        descUjian.banner = "";
         simpanBanner("");
         previewBanner("");
     }
